319c3353c79d5b85df640062ce6e27554dab88d8
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-15 20:41:04 +0300

    Encapsulation

---
+++ Demo/Encapsulation/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Encapsulation
+{
+    internal struct Employee
+    {
+        #region Attributes
+        private int Id;
+        private string? EmpName;
+        private decimal Empsalary;
+        #endregion
+        #region Constructors
+        public Employee(int id, string name, decimal salary, int age)
+        {
+            Id = id;
+            EmpName = name;
+            Salary = salary;
+        }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"{Id}\n{EmpName}\n{Empsalary:c}";
+        }
+        #endregion  
+        #region Setter Getter
+        public void SetName(string? value)
+        {
+            EmpName = value?.Length > 5 ? value.Substring(0, 5) : value;
+        }
+        public string? GetName()
+        {
+            return EmpName;
+        }
+        #endregion
+        #region Property
+        public decimal Salary
+        {
+            get { return Empsalary; }
+            set { Empsalary = value < 6000 ? 6000 : value; }
+        }
+        public int Age { get; set; } //Automatic Prop
+        public int age;
+
+        public int Agee //Full Prop
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        #endregion
+
+        //private decimal deduction; //Derived Attribute
+
+        public decimal Deduction
+        {
+            get { return Salary * 0.2M; }
+        }
+
+
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Demo
+﻿using Demo.Encapsulation;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Encapsulation
+            //Employee employee = new Employee(1,"Mohamed",3000 );
+            //Employee employee = new Employee(name: "Mohamed", id: 2, salary: 3000);
+            //employee.Id = 5;
+            //Console.WriteLine(employee.Id);
+
+            //employee.SetName("Mohamed");
+            //Console.WriteLine(employee.GetName());
+
+            //employee.Salary = 4000;
+            //Console.WriteLine(employee.Salary);
+
+            //Employee emp = new Employee(50, "Mohamed", 6000, 21);
+            //Console.WriteLine(emp.Deduction); 
+            #endregion
+
         }
     }
 }
