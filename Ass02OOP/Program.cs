20e79795d0f5b77a0399a962725e8f65e8eb0c49
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-16 06:01:30 +0300

    Q1

---
+++ Ass02OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace Ass02OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01 : Try all what we have learned in the lecture.
+            //Done in the Demo
+            #endregion
+
+        }
+    }
+}
