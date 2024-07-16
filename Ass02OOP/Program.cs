b676d47df65586000f78f6f03358076261fa6de8
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-16 14:29:20 +0300

    Solve Q2

--- Ass02OOP/Program.cs
+++ Ass02OOP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System;
+using System.Xml.Linq;
 
 namespace Ass02OOP
 {
@@ -10,7 +11,14 @@ namespace Ass02OOP
             #region Part01 : Try all what we have learned in the lecture.
             //Done in the Demo
             #endregion
-
+            #region Part02 - Q1 : Define a struct &quot;Person&quot; with properties &quot;Name&quot; and &quot;Age&quot;. Create an array of three &quot; Person & quot; objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+            //Person P = new Person(3);
+            //P.AddPerson(0, "Mohamed", 21);
+            //P.AddPerson(1, "Ahmed", 21);
+            //P.AddPerson(2, "Ali", 21);
+            //for (int i = 0; i < P.Size; i++)
+            //    Console.WriteLine(P[i]);
+            #endregion
         }
     }
 }
