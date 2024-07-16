87b66c228d09515e9c62763b963882e453b93021
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-16 17:08:55 +0300

    Solve Q6

--- Ass02OOP/Program.cs
+++ Ass02OOP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System;
 using System.Xml.Linq;
+using System.Reflection.Emit;
 
 namespace Ass02OOP
 {
@@ -20,24 +21,34 @@ namespace Ass02OOP
             //    Console.WriteLine(P[i]);
             #endregion
             #region Q2 : Create a struct called &quot;Person&quot; with properties &quot;Name&quot; and &quot;Age&quot;. Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
-            Personn[] people = new Personn[3];
-            for (int i = 0; i < people.Length; i++)
+            //Personn[] people = new Personn[3];
+            //for (int i = 0; i < people.Length; i++)
+            //{
+            //    Console.Write($"Name {i + 1}: ");
+            //    people[i].Name = Console.ReadLine();
+            //    Console.Write($"Age {i + 1}: ");
+            //    people[i].Age = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int Old = 0;
+            //for (int i = 1; i < people.Length; i++)
+            //{
+            //    if (people[i].Age > people[Old].Age)
+            //    {
+            //        Old = i;
+            //    }
+            //}
+            //Console.WriteLine($"Name: {people[Old].Name}");
+            //Console.WriteLine($"Age: {people[Old].Age}");
+            #endregion
+            #region Part03-Q6 : Design and implement a Class for the employees in a company:Employee is identified by an ID, Name, security level, salary, hire date and Gender.
+            Employee[] employees = new Employee[3];
+            employees[0] = Employee.CreateEmployee(SecurityPrivileges.DBA);
+            employees[1] = Employee.CreateEmployee(SecurityPrivileges.Guest);
+            employees[2] = Employee.CreateEmployee(SecurityPrivileges.DBA | SecurityPrivileges.Guest | SecurityPrivileges.Developer | SecurityPrivileges.Secretary);
+            foreach (Employee employee in employees)
             {
-                Console.Write($"Name {i + 1}: ");
-                people[i].Name = Console.ReadLine();
-                Console.Write($"Age {i + 1}: ");
-                people[i].Age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(employee);
             }
-            int Old = 0;
-            for (int i = 1; i < people.Length; i++)
-            {
-                if (people[i].Age > people[Old].Age)
-                {
-                    Old = i;
-                }
-            }
-            Console.WriteLine($"Name: {people[Old].Name}");
-            Console.WriteLine($"Age: {people[Old].Age}");
             #endregion
         }
     }
